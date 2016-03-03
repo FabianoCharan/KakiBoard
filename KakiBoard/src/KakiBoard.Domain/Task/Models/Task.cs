c749c5653d0f826eaa020810600b3c03068a997f
Alessandrocham <alessandrodba@outlook.com>
2016-03-03 17:21:48 -0300

    Criando Entidades no Domain

---
+++ KakiBoard/src/KakiBoard.Domain/Task/Models/Task.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KakiBoard.Domain.Task.Models
+{
+    public class Task
+    {
+        public string Titulo { get; private set; }
+        public string Descricao { get; private set; }
+        public string Tipo { get; private set; }
+        public string Projeto { get; private set; }
+        public string Tags { get; private set; }
+        public string ResponsavelNegocio { get; private set; }
+        public string ResponsavelTecnico { get; private set; }
+        public string QuemCadastrou { get; private set; }
+        public DateTime DataCadastro { get; set; }
+        public string Status { get; private set; }
+        public int TempoEstimado { get; private set; }
+        public int TempoGasto { get; private set; }
+        public int MyProperty { get; set; }
+
+        public Task()
+        {
+
+        }
+    }
+}
---
+++ KakiBoard/src/KakiBoard.Domain/User/Models/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KakiBoard.Domain.User.Models
+{
+    public class User
+    {
+        public string Nome { get; private set; }
+        public string Email { get; private set; }
+        public string Senha { get; private set; }
+
+        protected User() {}
+
+        public User(string nome,string email,string senha)
+        {
+            Nome = nome;
+            Email = email;
+            Senha = senha;
+        }
+    }
+
+
+
+}
