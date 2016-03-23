36ad79bac72a183e827db9efa39cbecd45c4bd41
Fabiano Charan <fabianocharan@gmail.com>
2016-03-23 18:52:37 -0300

    Add file TarefaAppService

---
+++ KakiBoard/src/KakiBoard.ApplicationService/Tarefa/TarefaApplicationService.cs
@@ -0,0 +1,45 @@
+﻿using KakiBoard.Domain.Tarefa.Services;
+using System;
+using KakiBoard.Domain.Tarefa.Commands;
+using KakiBoard.Domain.Tarefa.Repositories;
+using KakiBoard.SharedKernel.Repositories;
+
+namespace KakiBoard.ApplicationService.Tarefa
+{
+    public class TarefaApplicationService : ApplicationService, ITarefaApplicationService
+    {
+        private ITarefaRepository _repository;
+
+        public TarefaApplicationService(ITarefaRepository repository, IUnitOfWork unitOfWork)
+            :base(unitOfWork)
+        {
+            _repository = repository;
+        }
+
+        public Domain.Tarefa.Models.Tarefa AdicionarTarefa(TarefaCommand command)
+        {
+            var tarefa = new Domain.Tarefa.Models.Tarefa(command.Titulo, command.Tipo, command.Projeto, command.QuemCadastrou, command.Status);
+
+            var ok = "";
+
+            tarefa.AdicionarTarefa();
+
+            if (Commit())
+            {
+                return tarefa;
+            }
+
+            return null;
+        }
+
+        public Domain.Tarefa.Models.Tarefa AtribuirTarefa(TarefaCommand command)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Domain.Tarefa.Models.Tarefa EntregarTarefa(TarefaCommand command)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
