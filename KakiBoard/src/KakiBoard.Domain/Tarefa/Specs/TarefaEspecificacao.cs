3550d5028645483673bc9a8932efa0df5445c05d
Fabiano Charan <fabianocharan@gmail.com>
2016-03-24 11:58:02 -0300

    Tarefa Specs

---
+++ KakiBoard/src/KakiBoard.Domain/Tarefa/Specs/TarefaEspecificacao.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KakiBoard.Domain.Tarefa.Specs
+{
+    public class TarefaEspecificacao
+    {
+
+    }
+}
